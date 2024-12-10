0d18f70c197672eecb4609d737a0d900ac53748d
Beryl1808 <dkwafo17@gmail.com>
2024-12-10 19:44:17 +0000

    2commit

---
+++ Profit loss.py
@@ -0,0 +1,9 @@
+costprice =int(input("enter the cp:"))
+sellingprice =int(input("enter the sp:"))
+
+if(sellingprice>costprice):
+    print("print")
+    pt=sellingprice-costprice
+    print(pt)
+else :
+    print("No print")
